9d3b0df8f9ebb87d248864b08f84b71c6ceed43c
CraftException <craftexception@gmail.com>
2022-02-03 14:08:50 +0100

    Major Improvements to Design

--- static/scripts/navbar.ts
+++ static/scripts/navbar.ts
@@ -9,9 +9,9 @@ function updateNavBar() {
         document.getElementById("navHome").style.boxShadow = "0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19)";
         document.getElementById("navHome").style.backgroundColor = "#0d0d0d";
     } else {
-        document.getElementById("navHome").style.paddingTop = "20px";
-        document.getElementById("navHome").style.paddingBottom = "20px";
-        document.getElementById("navHome").style.backgroundColor = "transparent";
+        document.getElementById("navHome").style.paddingTop = "8px";
+        document.getElementById("navHome").style.paddingBottom = "8px";
+        document.getElementById("navHome").style.backgroundColor = "#303030";
         document.getElementById("navHome").style.boxShadow = "";
     }
 }
